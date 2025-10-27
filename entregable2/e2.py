b164609fa989b82c6722d1c3ff01e97b319c0ba4
Alex <al450630@uji.es>
2025-10-27 19:35:49 +0100

    Archivos entregable 2

---
+++ entregable2/e2.py
@@ -0,0 +1,26 @@
+import sys
+from typing import TextIO
+
+type Data = tuple[int, list[int]]
+type Result = tuple[int, int, int] | None
+
+
+def read_data(f: TextIO) -> Data:
+    pass
+
+
+def process(data: Data) -> Result:
+    pass
+
+
+def show_result(result: Result):
+    if result is None:
+        print("NO PROFIT")
+    else:
+        print(" ".join(str(x) for x in result))
+
+
+if __name__ == "__main__":
+    data0 = read_data(sys.stdin)
+    result0 = process(data0)
+    show_result(result0)
