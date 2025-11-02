a04910be3015712c39e22b1efde46e526bf1eabb
Alex <al450630@uji.es>
2025-11-02 04:55:33 +0100

    Función process con coste n^2

--- entregable2/e2.py
+++ entregable2/e2.py
@@ -1,6 +1,8 @@
 import sys
 from typing import TextIO
 
+from algoritmia.utils import infinity
+
 type Data = tuple[int, list[int]]
 type Result = tuple[int, int, int] | None
 
@@ -12,11 +14,13 @@ def read_data(f: TextIO) -> Data:
 
 
 def process(data: Data) -> Result:
+
+    n_dias, cot = data
     def rec(b: int, e: int):
         # if is_simple
         if e - b == 1:
             # return trivial solution
-            return None
+            return 0, -1, -1
         else:
             # divide
             mid = (b + e) // 2
@@ -24,9 +28,32 @@ def process(data: Data) -> Result:
             best_right = rec(mid, e)
 
             # combine
+            gan = 0
+            gan_max = 0
+            indice_compra = -1
+            indice_vente = -1
 
+            for i in range (b, mid):
+                for j in range (mid, e):
+                    if j - i <= n_dias:
+                        gan = cot[j] - cot[i]
+                        if gan > gan_max:
+                            gan_max = gan
+                            indice_compra = i
+                            indice_vente = j
 
+            best_mid = (gan_max, indice_compra, indice_vente)
 
+            return max(best_left, best_mid, best_right)
+
+    sol = rec(0, len(cot))
+
+    gan_max, indice_compra, indice_vente = sol
+
+    if gan_max > 0:
+        return sol
+    else:
+        return None
 
 def show_result(result: Result):
     if result is None:
